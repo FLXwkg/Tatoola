bf11bb48e3bdf7755bde972081d5426b79866a7c
FLXwkg <ewenheas13@gmail.com>
2024-02-09 11:49:35 +0100

    :art: : Tests affichage web

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  useWindowDimensions,
+} from 'react-native';
 
 export default function App() {
+  const {height} = useWindowDimensions();
+  const [number, setNumber] = useState(0);
+
+  function handlePress() {
+    setNumber(parseInt(Math.random() * 10000, 10) % 100);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!
-      Bonjour</Text>
-      <Text>Salut</Text>
-      <Text>Salut</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
+      <Text>Random number: {number}</Text>
+      <View style={styles.br} />
+      <Pressable
+        style={({pressed}) => [
+          {
+            opacity: pressed ? 0.7 : 1,
+          },
+          styles.btn,
+        ]}
+        onPress={handlePress}>
+        <Text style={styles.btnText}>Generate a number</Text>
+      </Pressable>
     </View>
   );
 }
@@ -19,5 +39,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  br: {
+    height: 12,
+  },
+  btn: {
+    backgroundColor: '#222',
+    padding: 10,
+  },
+  btnText: {
+    color: '#fff',
   },
 });
