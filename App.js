ef1365a4a18146d7a9763c12e1e1c13790f65793
FLXwkg <ewenheas13@gmail.com>
2024-03-01 10:48:01 +0100

    :art: : Ajout ImageViewer

--- App.js
+++ App.js
@@ -1,34 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
+  Image,
   Pressable,
   useWindowDimensions,
-} from 'react-native';
+} from "react-native";
+
+import Button from "./src/components/Buttons/Button";
+import ImageViewer from "./src/components/Pictures/ImageViewer";
+const PlaceholderImage = require("./assets/icon.png");
+
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  const {height} = useWindowDimensions();
-  const [number, setNumber] = useState(0);
-
-  function handlePress() {
-    setNumber(parseInt(Math.random() * 10000, 10) % 100);
-  }
-
   return (
-    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
-      <Text>Random number: {number}</Text>
-      <View style={styles.br} />
-      <Pressable
-        style={({pressed}) => [
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
-          styles.btn,
-        ]}
-        onPress={handlePress}>
-        <Text style={styles.btnText}>Generate a number</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer source={PlaceholderImage} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -36,18 +32,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  br: {
-    height: 12,
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
-  btn: {
-    backgroundColor: '#222',
-    padding: 10,
-  },
-  btnText: {
-    color: '#fff',
+  buttonContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
