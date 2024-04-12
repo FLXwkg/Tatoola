87ab854ca1f077de574cdeb072e2b6f2d3504230
FLXwkg <ewenheas13@gmail.com>
2024-04-12 14:23:34 +0200

    :art: Modification validation

--- backend/middlewares/userValidation.js
+++ backend/middlewares/userValidation.js
@@ -1,14 +1,13 @@
-// middlewares/userValidation.js
 const { check } = require('express-validator');
 
 const userValidationRules = [
-  check('name').notEmpty(),
-  check('lastname').notEmpty(),
-  check('email').isEmail(),
-  check('password').notEmpty(),
-  check('description').optional().isString(),
-  check('is_artist').isBoolean(),
-  check('is_premium').isBoolean()
+  check('name').notEmpty().withMessage('Name is required'),
+  check('lastname').notEmpty().withMessage('Lastname is required'),
+  check('email').isEmail().withMessage('Email is not valid'),
+  check('password').notEmpty().withMessage('Password is required'),
+  check('description').optional().isString().withMessage('Description must be a string'),
+  check('is_artist').isBoolean().withMessage('is_artist must be a boolean'),
+  check('is_premium').isBoolean().withMessage('is_premium must be a boolean')
 ];
 
 module.exports = {
