ac013752c1035a8cccc45298113afbd10f7a9caa
Yahlex <alexisremyh@gmail.com>
2024-04-12 14:33:35 +0200

    :art: ajout de la homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/header/Header';
+import SearchBar from '../components/SearchBar';
+import TattooInspiration from '../components/TattooInspiration';
+import LatestArticles from '../components/LatestArticles';
+import NearbyTattooists from '../components/NearbyTattooists';
+import Footer from '../components/footer/Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+      <SearchBar />
+      <div className="container">
+        <div className="mt-8 mb-8">
+          <h2 className="text-xl font-bold">Trouver votre tatouage</h2>
+          <TattooInspiration />
+          <Link to="/tatouages" className="text-blue-500">Voir tout</Link>
+        </div>
+        <div className="mt-8 mb-8">
+          <h2 className="text-xl font-bold">Les derniers articles du mag</h2>
+          <LatestArticles />
+        </div>
+        <div className="mt-8 mb-8">
+          <h2 className="text-xl font-bold">Les tatoueurs proches de chez vous</h2>
+          <NearbyTattooists />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/SearchPage.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import TattooList from '../components/TattooList';
+
+const SearchPage = () => {
+  // State pour gérer les critères de recherche
+  const [searchCriteria, setSearchCriteria] = useState({
+    location: '',
+    tattooStyle: '',
+  });
+
+  // Fonction pour mettre à jour les critères de recherche
+  const updateSearchCriteria = (criteria) => {
+    setSearchCriteria(criteria);
+  };
+
+  return (
+    <div>
+      <h1>Recherche de Tatoueurs</h1>
+      {/* Barre de recherche */}
+      <SearchBar updateSearchCriteria={updateSearchCriteria} />
+
+      {/* Liste des tatoueurs */}
+      <TattooList searchCriteria={searchCriteria} />
+    </div>
+  );
+};
+
+export default SearchPage;
