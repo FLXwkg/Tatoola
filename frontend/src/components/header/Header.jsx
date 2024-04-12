b06dd54a95c89116162aa49163f17f1ef7d291e3
Yahlex <alexisremyh@gmail.com>
2024-04-12 15:51:32 +0200

    :art: install next ui navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,28 +1,67 @@
-// Header.jsx
-import React from 'react';
-import { Button } from '@nextui-org/react';
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
+import {Button} from "@nextui-org/react";
 
+function Header() {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Tattoola</p>
+      </NavbarBrand>
 
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Trouver un tatoueur
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Inspiration tatouage
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Tatoomag
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
 
-function Header () {
-    return (
-      <header className='main'>
-        <nav>
-          <a href='/'>Accueil</a>
-          <a href='/artisans'>Artisans</a>
-          <a href='/about'>About</a>
-          <a href='/services'>Services</a>
-          <a href='/contact'>Contact</a>
-        </nav>
-        <a href='/authentication'>
-          <Button>
-            Connexion
-          </Button>
-        </a>
-      </header>
-    )
-  }
-  
-  export default Header;
+      <NavbarContent as="div" justify="end">
+        <Button>
+          Vous êtes tatoueur ?
+        </Button>
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+  );
+}
 
-
+export default Header;
