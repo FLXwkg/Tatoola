38746ed621d3f881399b456ab36bab926bdc29ff
FLXwkg <ewenheas13@gmail.com>
2024-05-03 11:10:02 +0200

    :alien: Fix middlewares

--- backend/middlewares/pictureValidation.js
+++ backend/middlewares/pictureValidation.js
@@ -3,5 +3,6 @@ const { body } = require('express-validator');
 exports.pictureValidationRules = [
   body('title').notEmpty().withMessage('Title is required'),
   body('description').optional().isString().withMessage('Description must be a string'),
-  body('image_url').notEmpty().withMessage('Image URL is required')
+  body('image_url').notEmpty().withMessage('Image URL is required'),
+  body('id_user').notEmpty().withMessage('User ID is required'),
 ];
