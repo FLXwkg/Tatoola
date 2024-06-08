528fdd962ef5f34e9f4f1c5b2dbff3fbab600524
FLXwkg <ewenheas13@gmail.com>
2024-06-08 18:51:32 +0200

    :art: Ajout pages connexion et dashboard + context

--- frontend/src/navigation/Router.jsx
+++ frontend/src/navigation/Router.jsx
@@ -13,6 +13,9 @@ import TattooTalk from '../pages/TattooTalk'
 import TattooMag from '../pages/TattooMag'
 import Map from '../components/map/Map' // Import de la carte
 import StylePage from '../pages/StylePage'
+import PrivateRoutes from './PrivateRouteMiddleware'
+import Dashboard from '../pages/protected/Dashboard'
+import Auth from '../pages/Auth'
 // Importez ici d'autres pages si nécessaire
 
 const Router = () => {
@@ -26,6 +29,10 @@ const Router = () => {
         <Route path='/tattoo-talk' element={<TattooTalk />} />
         <Route path='/map' element={<Map />} /> {/* Ajout de la route pour la carte */}
         <Route path='/style/:style' element={<StylePage />} /> {/* Ajout de la route pour la page de style  */}
+        <Route path='authentication' element={<Auth />} />
+        <Route path='dashboard' element={<PrivateRoutes />}>
+          <Route index element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
